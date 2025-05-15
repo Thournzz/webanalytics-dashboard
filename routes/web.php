7b1b7546b97c19315af113c36723dc014ecfe11b
Jourdan Farquharson <jourdan.farquharson@gmail.com>
2025-05-15 16:03:27 -0500

    Add dashboard view

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
+Route::get('/test', function () {
+    return view('index');
+});
+
 Route::get('/', fn() => view('index'));
 Route::get('/dashboard', [DashboardController::class, 'show']);
